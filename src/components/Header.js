dd6e5278c328fc72e7abd9d997f85fc8ed8456ef
Thomas Campbell <thomas.campbell@ecs.co.uk>
2022-10-18 18:46:17 +0100

    added Header to

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Header() {
+    return (
+    <header>
+    <nav>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="books.html">Books</a></li>
+        <li><a href="exercise.html">Exercise</a></li>
+        <li><a href="finance.html">Finance</a></li>
+        <li><a href="music.html">Music</a></li>
+        <li><a href="nutrition.html">Nutrition</a></li>
+        <li><a href="tech.html">Tech</a></li>
+        <li><a href="media.html">TV & Movies</a></li>
+      </ul>
+    </nav>
+</header>
+    );
+}
+
+export default Header; 
